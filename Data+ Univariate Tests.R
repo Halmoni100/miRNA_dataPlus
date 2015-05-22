d41e780a079ce5bc9bb5156e492a048cc064f5ff
kelseysumner <sumner.kelsey@gmail.com>
2015-05-22 15:09:20 -0400

    updated anova

--- Data+ Univariate Tests.R
+++ Data+ Univariate Tests.R
@@ -4,19 +4,28 @@ View(known_miRNAs_expressed_all_samples)
 
 
 
-# remove all non-normalized sample columns
-known_miRNAs_expressed_all_samples[57:108] <- list(NULL)
-
-
 
 #subset vector for aaa into virus, healthy & bacteria
-aaa_norm_subset <- known_miRNAs_expressed_all_samples[1]
-v <- aaa_norm_subset[,5:25]
-h <- aaa_norm_subset[,26:46]
-b <- aaa_norm_subset[,47:56]
+virus <- known_miRNAs_expressed_all_samples[1,5:25]
+baseline <- known_miRNAs_expressed_all_samples[1,26:46]
+bacteria <- known_miRNAs_expressed_all_samples[1,47:56]
+infection_status <- c(virus, baseline, bacteria)
+
+
+
 
 #run 1-way anova (completely randomized design)
-fit <- aov(y ~ A, data=aaa_norm_subset)
+1anova = lm()
+
+
+
+#t-test
+
+
+
+#wilcoxin
+
+
 
 # run Fisher's F-test if we want
 var.test(a,b)
