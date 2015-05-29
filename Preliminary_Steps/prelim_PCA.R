9cd52110e83fa0c6618998e0d1cbd05b53559494
Halmoni100 <6marc6100@gmail.com>
2015-05-29 16:32:29 -0400

    Fixed prelim_PCA.r
    Conflicting changes made some unwanted changes, fixed PCA code so it
    works now.

--- Preliminary_Steps/prelim_PCA.R
+++ Preliminary_Steps/prelim_PCA.R
@@ -1,17 +1,27 @@
-
 # Retrieve Data
 # raw_data retreived
 load("R_Data/saved_raw_data")
 
+# function to determine if all zeros
+all_zeros <- function(v) {
+    sum <- sum(v)
+    if (sum == 0) {
+        return(TRUE)
+    }
+    return(FALSE)
+}
+# Delete data w/ no info
+# Mark and delete all miRNAs with only zeros
+only_zeros <- apply(raw_data, 1, all_zeros)
+raw_data <- raw_data[!only_zeros,]
+
 #transpose data for PCA analysis
 raw_data_trans <- t(raw_data)
 dimnames(raw_data_trans) <- c(NULL, NULL)
-str(raw_data_trans)
-pca1 = prcomp(raw_data_trans, scale. = TRUE)
 
 #PCA analysis
 #source:http://gastonsanchez.com/blog/how-to/2012/06/17/PCA-in-R.html
-pca1 = prcomp(raw_data, scale. = TRUE)
+pca1 = prcomp(raw_data_trans, scale. = TRUE)
 
 # Graph PCA
 pca_result <- pca1$x
@@ -30,7 +40,6 @@ pca_comp_3_baseline <- pca_comp_3[22:42]
 pca_comp_3_bacteria <- pca_comp_3[43:52]
 
 # PC 1 vs PC 2
-pca_comp_3_bacteria
 plot(pca_comp_1_viral,pca_comp_2_viral,col="red",
 		xlim=c(min(pca_comp_1), max(pca_comp_1)),
 		ylim=c(min(pca_comp_2), max(pca_comp_2)))
@@ -53,4 +62,3 @@ plot(pca_comp_2_viral,pca_comp_3_viral,col="red",
 points(pca_comp_2_baseline,pca_comp_3_baseline,col="green")
 points(pca_comp_2_bacteria,pca_comp_3_bacteria,col="blue")
 points(pca_comp_2_bacteria,pca_comp_3_bacteria,col="blue")
-
