d83791504b61e9eb17f3bc0a23492e80dbdf2b2c
kelseysumner <sumner.kelsey@gmail.com>
2015-06-03 11:20:32 -0400

    working on quantile normalization

--- Preliminary_Steps/Normalization.R
+++ Preliminary_Steps/Normalization.R
@@ -36,28 +36,38 @@ for (i in 1:ncol(processed_data)) {
 	uqnorm[,i] <- colneeded/Us[i]*U
 }
 
+#PCA Analysis on Normalized data by using log function
 logged <- log2(uqnorm + 1)
 pca_analysis <- dget("Preliminary_Steps/analyze_PCs.r")
 pca_analysis(logged, 25)
 
 
-# all code below here is adapted from http://davetang.org/muse/2014/07/07/quantile-normalisation-in-r/
+
 # create quantile normalization function
 quantile_normalization <- function(uqnorm) {
 	uqnorm_rank <- apply(uqnorm, 2, rank, ties.method="min")
-	uqnorm_sorted <- data.frame(apply(uqnorm, 2, sort))
-	uqnorm_mean <- apply(uqnorm_sorted, 1, mean)
+	uqnorm_sorted <- apply(uqnorm, 2, sort)
 	
-	index_to_mean <- function (my_index, my_mean) {
-		return (my_mean[my_index])
+	#create the sampling function to add into the quantiles function
+	sampling_vector <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52)
+	sampling <- function(x) {
+		divide <- x/53
+		return(divide)
 	}
 	
-	uqnorm_final <- apply(uqnorm_rank, 2, index_to_mean, my_mean = uqnorm_mean)
-	return(uqnorm_final)
-}
+	makingquantiles <- sapply(sampling_vector,sampling)	
+	
+	uqnorm_final <- function(uqnorm_sorted) {
+		qnorm(makingquantiles, mean=0, sd=1)
+		return(uqnorm_final)
 
+	}
+	return(quantile_normalization)
+	}
+	
 # test the function
 quantile_norm_data <- quantile_normalization(uqnorm)
 
-# graph the normalized data on a histogram
-hist(quantile_norm_data)
+
+# save final normalized data
+save(quantile_norm_data, file="R_Data/saved_quantile_norm_data")
