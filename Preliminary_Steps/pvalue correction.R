726dd98d2dda6eb673daf8caf6c0da5448f6783b
kelseysumner <sumner.kelsey@gmail.com>
2015-06-10 13:50:37 -0400

    adjustment methods

--- Preliminary_Steps/pvalue correction.R
+++ Preliminary_Steps/pvalue correction.R
@@ -1,7 +1,8 @@
 # compute Bonferroni p-values
-p.adjust(p, method = "bonferroni")
+badjustment <- p.adjust(INSERT PVALUE VECTOR HERE, method = "bonferroni")
 
 
 
 
 # compute false discovery rates 
+fdradjustment <- p.adjust(INSERT PVALUE VECTOR HERE, method = "fdr")
