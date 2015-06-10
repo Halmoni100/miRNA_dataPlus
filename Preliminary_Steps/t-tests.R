ea765b72dba1f8e1b7e3fe7cd22fac987ed052aa
kelseysumner <sumner.kelsey@gmail.com>
2015-06-10 10:51:53 -0400

    check the for loop :)

--- Preliminary_Steps/t-tests.R
+++ Preliminary_Steps/t-tests.R
@@ -1,58 +1,50 @@
-# load normalized data set
+# load normalized data set using "Normalization" R script
 
 
 # subset viral, bacterial, baseline, asymptomatic/symptomatic miRNA expression
-allviral <- uqnorm[,1:21]
-baseline <- uqnorm[,22:42]
-bacterial <- uqnorm[,43:52]
-viral_symptomatic_vector <- c(1,2,5,9,11,13,15,16,17,18,19,20,21)
-viral_asymptomatic_vector <- c(3,4,6,7,8,10,12,14)
-viral_symptomatic <- allviral[,viral_symptomatic_vector]
-viral_asymptomatic <- allviral[,viral_asymptomatic_vector]
-combined_bacterial_baseline <- uqnorm[,22:52]
-combined_bacterial_allviral_vector <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,43,44,45,46,47,48,49,50,51,52)
-combined_bacterial_allviral <- uqnorm[,combined_bacterial_allviral_vector]
-combined_baseline_allviral <- uqnorm[,1:42]
+allviral <- quantile_norm_data[,1:21]
+baseline <- quantile_norm_data[,22:42]
+bacterial <- quantile_norm_data[,43:52]
+viral_symptomatic <- quantile_norm_data[,c(1,2,5,9,11,13,15,16,17,18,19,20,21)]
+viral_asymptomatic <- quantile_norm_data[,c(3,4,6,7,8,10,12,14)]
+combined_bacterial_baseline <- quantile_norm_data[,22:52]
+combined_bacterial_allviral <- quantile_norm_data[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,43,44,45,46,47,48,49,50,51,52)]
+combined_baseline_allviral <- quantile_norm_data[,1:42]
 
 
 
-# calculate means of each variable
-mean_allviral <- mean(allviral)
-mean_bacterial <- mean(bacterial)
-mean_baseline <- mean(baseline)
-mean_viral_symptomatic <- mean(viral_symptomatic)
-mean_viral_asymptomatic <- mean(viral_asymptomatic)
 
-
-
-# standard deviation for each variable
-sd_allviral <- sd(allviral)
-sd_bacterial <- sd(bacterial)
-sd_baseline <- sd(baseline)
-sd_viral_symptomatic <- sd(viral_symptomatic)
-sd_viral_asymptomatic <- sd(viral_asymptomatic)
-
-
-
-# conducting a Welch Two Sample t-test on every variable (not assuming equal standard deviation)
-test1 <- t.test(allviral,bacterial,alternative="two.sided")
-test2 <- t.test(allviral,baseline,alternative="two.sided", paired=TRUE)
+# conducting a Welch Two Sample t-test on every variable (not assuming equal standard deviation) using a for loop
+for (k in 1:nrow(quantile_norm_data)) {
+output_vector <- vector()
+# make an output matrix for the pvalues of all the tests
+ttestmatrix <- matrix(, ncol=11, nrow=k)
+output_vector[1] <- t.test(allviral[,k],bacterial[,k],alternative="two.sided")
+output_vector[2] <- t.test(allviral[,k],baseline[,k],alternative="two.sided", paired=TRUE)
 # test2 is a paired t-test 
-test4 <- t.test(bacterial,baseline,alternative="two.sided")
-test5 <- t.test(bacterial,viral_symptomatic,alternative="two.sided")
-test6 <- t.test(bacterial,viral_asymptomatic,alternative="two.sided")
-test9 <- t.test(combined_bacterial_baseline,allviral,alternative="two.sided")
-test10 <- t.test(combined_bacterial_allviral,baseline,alternative="two.sided")
-test11 <- t.test(combined_baseline_allviral,bacterial,alternative="two.sided")
+output_vector[3] <- t.test(viral_symptomatic[,k], viral_asymptomatic[,k],alternative="two.sided")
+output_vector[4] <- t.test(bacterial[,k],baseline[,k],alternative="two.sided")
+output_vector[5] <- t.test(bacterial[,k],viral_symptomatic[,k],alternative="two.sided")
+output_vector[6] <- t.test(bacterial[,k],viral_asymptomatic[,k],alternative="two.sided")
+output_vector[7] <- t.test(baseline[,k], viral_symptomatic[,k], alternative="two.sided")
+output_vector[8] <- t.test(baseline[,k], viral_asymptomatic[,k], alternative="two.sided")
+output_vector[9] <- t.test(combined_bacterial_baseline[,k],allviral[,k],alternative="two.sided")
+output_vector[10] <- t.test(combined_bacterial_allviral[,k],baseline[,k],alternative="two.sided")
+output_vector[11] <- t.test(combined_baseline_allviral[,k],bacterial[,k],alternative="two.sided")
+
+ttestmatrix[k,] <- output_vector
+}
+
+
+
+print(ttestmatrix)
 
 
 
 
-# conducting a multiple linear regression model to compare baseline to viral_symptomatic and viral_asymptomatic
-symptom <- cbind(viral_symptomatic + viral_asymptomatic)
-test7 <- manova(symptom ~ baseline)
-boxplot(baseline ~ viral_symptomatic)
-boxplot(baseline ~ viral_asymptomatic)
+
+
+
 
 
 
