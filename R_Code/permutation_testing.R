dc5fe4040d52c819ccd02211ceb8fa69ca295457
Halmoni100 <6marc6100@gmail.com>
2015-06-12 12:05:10 -0400

    Small changes
    Slightly modified Normalization code

--- R_Code/permutation_testing.R
+++ R_Code/permutation_testing.R
@@ -8,6 +8,24 @@
 # Baseline - bl
 # Bacteria - bc
 
-# Assume that group 1 is class in consideration, group 2 is everything else
-groups <- rep.int(2, nrow(quantile_norm_data))
+
+create_truth_vec <- function(class) {
+	# Assume that group 1 is class in consideration, group 2 is everything else
+	truth_vec <- rep.int(2, nrow(quantile_norm_data))
+	truth_table <- sample_factors == class
+	for (i in 1:length(truth_vec)) {
+		if (truth_table[i]) {
+			truth_vec[i] <- 1
+		}
+	}
+}
+
+v_s_truth_vec <- create_truth_vec("v_s")
+v_as_truth_vec <- create_truth_vec("v_as")
+bl_truth_vec <- create_truth_vec("bl")
+bc_truth_vec <- create_truth_vec("bc")
+
+
+
+
 
