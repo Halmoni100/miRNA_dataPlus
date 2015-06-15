a9e672ad0acbf1a74ca24975402f28782dbc2c74
kelseysumner <sumner.kelsey@gmail.com>
2015-06-15 15:36:39 -0400

    bonferroni and FDR correction with significant pvalues chosen

--- R_Code/pvalue correction.R
+++ R_Code/pvalue correction.R
@@ -1,29 +1,84 @@
 
-# compute Bonferroni p-values (currently not using)
-b_vector <- c(p_vals)
-b_adjustment <- p.adjust(p_vals, method = "bonferroni")
-b_matrix <- matrix(b_adjustment, nrow=333, ncol=11)
+# compute Bonferroni p-values
+# set up a vector for each test(column)
+b_vector_test1 <- c(p_vals[,1])
+b_vector_test2 <- c(p_vals[,2])
+b_vector_test3 <- c(p_vals[,3])
+b_vector_test4 <- c(p_vals[,4])
+b_vector_test5 <- c(p_vals[,5])
+b_vector_test6 <- c(p_vals[,6])
+b_vector_test7 <- c(p_vals[,7])
+b_vector_test8 <- c(p_vals[,8])
+b_vector_test9 <- c(p_vals[,9])
+b_vector_test10 <- c(p_vals[,10])
+b_vector_test11 <- c(p_vals[,11])
+
+# run the bonferroni adjustment code on each vector
+b_adjustment1 <- p.adjust(b_vector_test1, method = "bonferroni")
+b_adjustment2 <- p.adjust(b_vector_test2, method = "bonferroni")
+b_adjustment3 <- p.adjust(b_vector_test3, method = "bonferroni")
+b_adjustment4 <- p.adjust(b_vector_test4, method = "bonferroni")
+b_adjustment5 <- p.adjust(b_vector_test5, method = "bonferroni")
+b_adjustment6 <- p.adjust(b_vector_test6, method = "bonferroni")
+b_adjustment7 <- p.adjust(b_vector_test7, method = "bonferroni")
+b_adjustment8 <- p.adjust(b_vector_test8, method = "bonferroni")
+b_adjustment9 <- p.adjust(b_vector_test9, method = "bonferroni")
+b_adjustment10 <- p.adjust(b_vector_test10, method = "bonferroni")
+b_adjustment11 <- p.adjust(b_vector_test11, method = "bonferroni")
+
+# combine the vector and put it back into a matrix
+b_combined <- c(b_adjustment1, b_adjustment2, b_adjustment3, b_adjustment4, b_adjustment5, b_adjustment6, b_adjustment7, b_adjustment8, b_adjustment9, b_adjustment10, b_adjustment11)
+b_matrix <- matrix(b_combined, nrow=333, ncol=11)
+
+# assign all signifiant p-values as TRUE after Bonferroni
+significant_b_p_vals <- b_matrix <= .05
+
+# pulling out significant miRNAs expressed
+# create a function to choose only the TRUE values (significant pvalues)
+takingout <- function(significant_b_p_vals) {
+	takeout <- which(significant_b_p_vals == TRUE)
+	return(takeout)
+}
+
+# use the apply function to create a list of significant miRNAs
+list_significant_miRNA <- apply(significant_b_p_vals, 2, takingout)
 
 
-# MY CODE FOR BONFERRONI
-# new threshold (Ask about)
-b_null <- .05/11
 
-# assign all signifiant p-values as TRUE
-significant_p_vals <- p_vals <= b_null
 
 
 
 
 # compute false discovery rates (less conservative method than the bonferroni correction)
-fdr_vector <- c(p_vals)
-fdr_adjustment <- p.adjust(p_vals, method = "fdr")
-fdr_matrix <- matrix(fdr_adjustment, nrow=333, ncol=11)
+# set up a vector for each test(column)
+fdr_vector_test1 <- c(p_vals[,1])
+fdr_vector_test2 <- c(p_vals[,2])
+fdr_vector_test3 <- c(p_vals[,3])
+fdr_vector_test4 <- c(p_vals[,4])
+fdr_vector_test5 <- c(p_vals[,5])
+fdr_vector_test6 <- c(p_vals[,6])
+fdr_vector_test7 <- c(p_vals[,7])
+fdr_vector_test8 <- c(p_vals[,8])
+fdr_vector_test9 <- c(p_vals[,9])
+fdr_vector_test10 <- c(p_vals[,10])
+fdr_vector_test11 <- c(p_vals[,11])
 
+# Run fdr_adjustment on each vector
+fdr_adjustment1 <- p.adjust(fdr_vector_test1, method = "fdr")
+fdr_adjustment2 <- p.adjust(fdr_vector_test2, method = "fdr")
+fdr_adjustment3 <- p.adjust(fdr_vector_test3, method = "fdr")
+fdr_adjustment4 <- p.adjust(fdr_vector_test4, method = "fdr")
+fdr_adjustment5 <- p.adjust(fdr_vector_test5, method = "fdr")
+fdr_adjustment6 <- p.adjust(fdr_vector_test6, method = "fdr")
+fdr_adjustment7 <- p.adjust(fdr_vector_test7, method = "fdr")
+fdr_adjustment8 <- p.adjust(fdr_vector_test8, method = "fdr")
+fdr_adjustment9 <- p.adjust(fdr_vector_test9, method = "fdr")
+fdr_adjustment10 <- p.adjust(fdr_vector_test10, method = "fdr")
+fdr_adjustment11 <- p.adjust(fdr_vector_test11, method = "fdr")
 
-#another method
-compute.FDR(p_vals, q)
+# combine the adjustment vectors and create a new pvalue matrix
+fdr_combined <- c(fdr_adjustment1, fdr_adjustment2, fdr_adjustment3, fdr_adjustment4, fdr_adjustment5, fdr_adjustment6, fdr_adjustment7, fdr_adjustment8, fdr_adjustment9, fdr_adjustment10, fdr_adjustment11)
+fdr_matrix <- matrix(fdr_combined, nrow=333, ncol=11)
 
-
-# MY CODE FOR FDR
-fdr_null <- (11*0.05)/2
+#assign all significant p-values as TRUE after FDR
+significant_fdr_p_vals <- fdr_matrix <= .05
