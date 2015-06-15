04f1a86d33485220da5db7b3512433253af72112
Halmoni100 <6marc6100@gmail.com>
2015-06-15 16:52:01 -0400

    Finished permutation testing, slight change to PCA
    Added two group permutation testing, also outputted correlation matrix
    in PCA

--- R_Code/PCA/pca_samples.R
+++ R_Code/PCA/pca_samples.R
@@ -1,7 +1,6 @@
-# Retrieve Data
-# quantile_norm_data retrieved
-load("R_data/saved_quantile_norm_data")
+# Load the following...
+# quantile_norm_data
 
 # Do PCA Analysis
-pca_analysis <- dget("Preliminary_Steps/PCA/analyze_PCs_samples.r")
+pca_analysis <- dget("R_Code/PCA/analyze_PCs_samples.r")
 pca_analysis(quantile_norm_data, 10)
